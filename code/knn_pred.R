e5577e5e70a5d63a28117dbb8d0d60d0d296b240
Dave Hurst <dave.a.hurst@seagate.com>
2016-05-22 15:55:45 -0700

    knn 1 - not run on full dataset (ran 20+ hrs before aborting)

--- code/knn_pred.R
+++ code/knn_pred.R
@@ -1,7 +1,29 @@
+library(class)
+library(dplyr)
+library(data.table)
+if (! grepl('\\/code$', getwd())) setwd('code')
+stopifnot (grepl('\\/code$', getwd()))
+
 read_class <- c("integer", "numeric", "numeric", "integer", "integer", "character")
 if (! exists("df_train")) df_train <- fread('../input/train.csv', colClasses = read_class)
-if (! exists("df_test")) df_train <- fread('../input/train.csv', colClasses = read_class)
+if (! exists("df_test")) df_test <- fread('../input/test.csv')
 
-fsize = .01
-df_samp <- df_train 
-if (fsize < 1) df_samp <- df_all %>% sample_frac(size=fsize)
+fsize = .05
+set.seed(19)
+if (fsize < 1) { 
+    trn <- df_train %>% sample_frac(size=fsize)
+    tst <- df_test  %>% sample_frac(size=fsize)
+} else {
+    trn <- df_train
+    tst <- df_test
+}
+
+normalize <- function(x) (x - min(x)) / diff(range(x))
+
+trn.truth <- trn$place_id %>% as.factor()
+trn$place_id <- NULL
+trn.norm <- trn %>% apply(2, normalize) %>% data.frame() %>% tbl_df()
+tst.norm <- tst %>% apply(2, normalize) %>% data.frame() %>% tbl_df()
+
+t0 <- proc.time()
+knn1 <- knn( trn.norm, tst.norm, trn.truth, k=1, prob=TRUE, use.all = TRUE); t1 <- proc.time()
