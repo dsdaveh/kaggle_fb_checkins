2c6ab9cf144f9e42b36b843a3be035d3a266b199
Dave Hurst <dave.a.hurst@seagate.com>
2016-06-06 19:03:40 -0700

    look at fractional scores

--- code/hectare_analysis2.R
+++ code/hectare_analysis2.R
@@ -16,13 +16,18 @@ load("../data/hectare_prob_loc_5fold_indices.RData")
 nhect <- 15
 
 set.seed(48)
-h_sample <- sample(1:100, nhect * 2, replace=TRUE) %>% matrix(nrow=nhect, ncol=2) %>% as.data.frame()
-names(h_sample) <- c('x', 'y')
+# h_sample <- sample(1:100, nhect * 2, replace=TRUE) %>% matrix(nrow=nhect, ncol=2) %>% as.data.frame()
+# names(h_sample) <- c('x', 'y')
+h_backlog <- expand.grid( x=1:100, y=1:100) %>% sample_frac(size=1)
+chunk_size = 1000
+chunk <- h_backlog[1:1000,]
+h_backlog <- h_backlog[-(1:1000),]
 
 plot(c(0,100), c(0,100), type="n")
-for( i in 1:nhect) with(h_sample[i, ], {
-    rect(x-1, y-1, x, y)
-    text(x, y, i, pos=4) } )
+for( i in 1:chunk_size) with(chunk[i, ], { rect(x-1, y-1, x, y) } )
+# for( i in 1:nhect) with(chunk[i, ], {
+#     rect(x-1, y-1, x, y)
+#     text(x, y, i, pos=4) } )
 
 create_features <- function(dt) {
     if (! "place_id" %in% names(dt)) dt$place_id <- "TBD"
@@ -46,7 +51,8 @@ train <- train[1:train_cut_ix, ]
 t0 <- proc.time()
 h_score <- numeric(nhect)
 h_stats <- data.frame()
-for( i in 1:nhect) {
+xgb_results <- data.frame()
+for( i in 1:chunk_size) {
     locij <- hp_summarize( train, h_sample[i,1], h_sample[i,2], min_time=0)
     hp_loc <- rbind(hp_loc, locij)
     xmin <- (h_sample[i,1] - 1) / 10.
@@ -93,6 +99,7 @@ for( i in 1:nhect) {
     
     ## xgb classifier
     xgb_preds <- hp_classify( trn, val, min_occ = 10)
+    xgb_results <- rbind( xgb_results, xgb_preds)
     score_xgb <- calculate_map_score( xgb_preds )
     cat(sprintf("%d: xgb score=%f\n", i, score_xgb))
     
@@ -177,120 +184,120 @@ hp_classify <- function(trn, val, min_occ=2) {
     
 #dev version
 # hp_summarize <- hp_summarize_dev
-hp_summarize_dev <- function(trn, hi, hj) {
-    # i,j = hectare id from 1-100 in x and y, respectively
-    stopifnot( hi > 0 & hi <= 100 & hj > 0 & hj <= 100)
-    
-    hdata <- trn %>% filter( x >= (hi-1)/10, x <= hi/10,
-                             y >= (hj-1)/10, y <= hj/10) 
-    
-    hdata$hour_abs <- as.integer(hdata$time / 60)
-    hdata$hour_in_day <- hdata$hour_abs %% 24
-    hdata$day_abs <- as.integer( hdata$time / (60 * 24) )
-    hdata$day_in_week <- hdata$day_abs %% 7
-    hdata$qtr_in_year <- as.integer(hdata$time / (60 * 24 * 91)) %% 4
-    
-    n_places <- length(unique(hdata$place_id))
-    
-    hdata$pid <- as.factor(hdata$place_id)
-    levels_orig <- levels(hdata$pid)
-    new_ids <- expand.grid(LETTERS, letters, 0:9) %>% 
-        mutate(id = paste0(Var1, Var2, Var3)) %>% .[[4]] %>%
-        head( length(levels_orig))
-    levels(hdata$pid) <- new_ids
-    
-    # look for a reasonable threshhold for n_occ ( < 20 place_ids )
-    thresh <- hdata %>% count(place_id, sort=TRUE) %>% 
-        rename(n_occ=n) %>% count(n_occ) %>% 
-        arrange(n_occ) %>%
-        mutate( n_cutoff = sum(n) - cumsum(n)) 
-    thresh
-    thresh %>% ggplot(aes(n_occ, n_cutoff)) + geom_bar(stat="identity")
-    
-    #find places with more than n occurances in grid
-    n_occ <- 10
-    mults <- hdata %>% count(place_id, sort=TRUE) %>% filter(n > n_occ) %>% .[[1]]
-
-    hlimit <- length(mults)
-    hdata %>% filter(place_id %in% mults[1:hlimit]) %>%
-        ggplot( aes(pid)) + geom_bar() + coord_flip()
-    
-    
-    
-    
-    #for h=98,62:
-    # number of places in validation set = 457 | nrow(val)
-    # number of which are in training set = 435 (95%) | sum(val$place_id %in% hdata$place_id)
-    # number in top10 training = 271 (59%) sum(val$place_id %in% mults[1:10])
-    # number in top20 = 347 (76%), top50 = 410 (88%)
-    
-    # list of place ids that don't occur after day 300 (range = 0, 545)
-    defunct <- hdata %>% group_by(place_id) %>%
-        summarise( last_day = max(day_abs)) %>%
-        filter( last_day <= 300 ) %>%
-        .[[1]]
-    hdata %>% filter(place_id %in% defunct) %>%
-        ggplot( aes(day_abs, pid, col=pid)) + geom_jitter(width=0, height=.1, alpha=.5) + guides(col=FALSE)
-    #number of validates after day 400 that use these  #note validate was prefiltered here (Not passed in) =2
-    val %>% mutate(day_abs = as.integer( time / (60 * 24) )) %>%
-        filter(day_abs > 400, place_id %in% defunct) %>% nrow()  
-    
-    ## Hmmm:  use data after 400 days to predict? More closely models test data?
-    val400 <- val %>% mutate(day_abs = as.integer( time / (60 * 24) )) %>% filter(day_abs > 400)
-    mults400 <- mults[! mults %in% defunct]
-    # number of places in validation set after  = 179 | nrow(val400)
-    # number of which are in training set = 169 (94%) | sum(val400$place_id %in% hdata$place_id)
-    # number in top10 training = 99 (55%) | sum(val400$place_id %in% mults[1:10])
-    # number in top20 = 130 (72%) , top50 = 157 (88%)
-    
-    centroids <- hdata %>% group_by(pid, place_id) %>%
-        summarize( x = mean(x),
-                   y = mean(y),
-                   n = n()) %>%
-        mutate(type = 'centroid') %>%
-        ungroup %>% arrange(desc(n))
-    
-    grp <- hdata %>% filter(place_id %in% mults[1:10]) %>% mutate(n=1, type='event')  %>% select(pid, place_id, x, y, n, type, day_abs) 
-    bind_rows(grp,centroids[1:10,]) %>% ggplot( aes(x, y, group=pid, col=pid)) + 
-        geom_point(aes(size=n, shape=as.factor(type))) 
-   
-    grp %>% ggplot( aes(day_abs, pid, col=pid)) + geom_jitter(width=0, height=.3, alpha=.5)
-    
-    grp <- hdata %>% filter(place_id %in% mults[31:40])
-    grp %>% ggplot( aes(x, y, group=pid, col=pid)) + geom_point()
-    grp %>% ggplot( aes(day_abs, pid, col=pid)) + geom_jitter(width=0, height=.3, alpha=.5)
-
-    plot(val400$x, val400$y, col=as.factor(val400$place_id), pch=16) 
-    
-    
-    
-    
-    ncut <- min(3, n_places)
-    
-    hprob <- hdata %>%
-        group_by(hour_abs, place_id) %>%
-        summarise(n = n()) %>%
-        arrange(hour_abs)
-    
-        count(place_id, sort=TRUE) %>% 
-        mutate(P = n / sum(n)) %>%
-        head(ncut) %>% 
-        mutate(  rank = 1:ncut) 
-    
-    if(ncut < 3 )  hprob <- hprob %>% bind_rows( hprob[1,] %>% mutate( rank = 3))
-    if(ncut < 2 )  hprob <- hprob %>% bind_rows( hprob[1,] %>% mutate( rank = 2)) %>%
-        mutate(P = n / sum(n))
-    
-    top_places <- hprob %>% 
-        mutate( place_rank = paste0("place", rank)) %>%
-        dplyr::select(place_id, place_rank) %>%
-        tidyr::spread(place_rank, place_id)
-    
-    top_probs <- hprob %>% 
-        mutate( prob_rank = paste0("P", rank)) %>%
-        dplyr::select(P, prob_rank) %>%
-        tidyr::spread(prob_rank, P)
-    
-    data.frame( h_x = hi, h_y = hj, P3cum = sum(hprob$P), n_places = n_places) %>%
-        bind_cols( top_places, top_probs) 
-}
+# hp_summarize_dev <- function(trn, hi, hj) {
+#     # i,j = hectare id from 1-100 in x and y, respectively
+#     stopifnot( hi > 0 & hi <= 100 & hj > 0 & hj <= 100)
+#     
+#     hdata <- trn %>% filter( x >= (hi-1)/10, x <= hi/10,
+#                              y >= (hj-1)/10, y <= hj/10) 
+#     
+#     hdata$hour_abs <- as.integer(hdata$time / 60)
+#     hdata$hour_in_day <- hdata$hour_abs %% 24
+#     hdata$day_abs <- as.integer( hdata$time / (60 * 24) )
+#     hdata$day_in_week <- hdata$day_abs %% 7
+#     hdata$qtr_in_year <- as.integer(hdata$time / (60 * 24 * 91)) %% 4
+#     
+#     n_places <- length(unique(hdata$place_id))
+#     
+#     hdata$pid <- as.factor(hdata$place_id)
+#     levels_orig <- levels(hdata$pid)
+#     new_ids <- expand.grid(LETTERS, letters, 0:9) %>% 
+#         mutate(id = paste0(Var1, Var2, Var3)) %>% .[[4]] %>%
+#         head( length(levels_orig))
+#     levels(hdata$pid) <- new_ids
+#     
+#     # look for a reasonable threshhold for n_occ ( < 20 place_ids )
+#     thresh <- hdata %>% count(place_id, sort=TRUE) %>% 
+#         rename(n_occ=n) %>% count(n_occ) %>% 
+#         arrange(n_occ) %>%
+#         mutate( n_cutoff = sum(n) - cumsum(n)) 
+#     thresh
+#     thresh %>% ggplot(aes(n_occ, n_cutoff)) + geom_bar(stat="identity")
+#     
+#     #find places with more than n occurances in grid
+#     n_occ <- 10
+#     mults <- hdata %>% count(place_id, sort=TRUE) %>% filter(n > n_occ) %>% .[[1]]
+# 
+#     hlimit <- length(mults)
+#     hdata %>% filter(place_id %in% mults[1:hlimit]) %>%
+#         ggplot( aes(pid)) + geom_bar() + coord_flip()
+#     
+#     
+#     
+#     
+#     #for h=98,62:
+#     # number of places in validation set = 457 | nrow(val)
+#     # number of which are in training set = 435 (95%) | sum(val$place_id %in% hdata$place_id)
+#     # number in top10 training = 271 (59%) sum(val$place_id %in% mults[1:10])
+#     # number in top20 = 347 (76%), top50 = 410 (88%)
+#     
+#     # list of place ids that don't occur after day 300 (range = 0, 545)
+#     defunct <- hdata %>% group_by(place_id) %>%
+#         summarise( last_day = max(day_abs)) %>%
+#         filter( last_day <= 300 ) %>%
+#         .[[1]]
+#     hdata %>% filter(place_id %in% defunct) %>%
+#         ggplot( aes(day_abs, pid, col=pid)) + geom_jitter(width=0, height=.1, alpha=.5) + guides(col=FALSE)
+#     #number of validates after day 400 that use these  #note validate was prefiltered here (Not passed in) =2
+#     val %>% mutate(day_abs = as.integer( time / (60 * 24) )) %>%
+#         filter(day_abs > 400, place_id %in% defunct) %>% nrow()  
+#     
+#     ## Hmmm:  use data after 400 days to predict? More closely models test data?
+#     val400 <- val %>% mutate(day_abs = as.integer( time / (60 * 24) )) %>% filter(day_abs > 400)
+#     mults400 <- mults[! mults %in% defunct]
+#     # number of places in validation set after  = 179 | nrow(val400)
+#     # number of which are in training set = 169 (94%) | sum(val400$place_id %in% hdata$place_id)
+#     # number in top10 training = 99 (55%) | sum(val400$place_id %in% mults[1:10])
+#     # number in top20 = 130 (72%) , top50 = 157 (88%)
+#     
+#     centroids <- hdata %>% group_by(pid, place_id) %>%
+#         summarize( x = mean(x),
+#                    y = mean(y),
+#                    n = n()) %>%
+#         mutate(type = 'centroid') %>%
+#         ungroup %>% arrange(desc(n))
+#     
+#     grp <- hdata %>% filter(place_id %in% mults[1:10]) %>% mutate(n=1, type='event')  %>% select(pid, place_id, x, y, n, type, day_abs) 
+#     bind_rows(grp,centroids[1:10,]) %>% ggplot( aes(x, y, group=pid, col=pid)) + 
+#         geom_point(aes(size=n, shape=as.factor(type))) 
+#    
+#     grp %>% ggplot( aes(day_abs, pid, col=pid)) + geom_jitter(width=0, height=.3, alpha=.5)
+#     
+#     grp <- hdata %>% filter(place_id %in% mults[31:40])
+#     grp %>% ggplot( aes(x, y, group=pid, col=pid)) + geom_point()
+#     grp %>% ggplot( aes(day_abs, pid, col=pid)) + geom_jitter(width=0, height=.3, alpha=.5)
+# 
+#     plot(val400$x, val400$y, col=as.factor(val400$place_id), pch=16) 
+#     
+#     
+#     
+#     
+#     ncut <- min(3, n_places)
+#     
+#     hprob <- hdata %>%
+#         group_by(hour_abs, place_id) %>%
+#         summarise(n = n()) %>%
+#         arrange(hour_abs)
+#     
+#         count(place_id, sort=TRUE) %>% 
+#         mutate(P = n / sum(n)) %>%
+#         head(ncut) %>% 
+#         mutate(  rank = 1:ncut) 
+#     
+#     if(ncut < 3 )  hprob <- hprob %>% bind_rows( hprob[1,] %>% mutate( rank = 3))
+#     if(ncut < 2 )  hprob <- hprob %>% bind_rows( hprob[1,] %>% mutate( rank = 2)) %>%
+#         mutate(P = n / sum(n))
+#     
+#     top_places <- hprob %>% 
+#         mutate( place_rank = paste0("place", rank)) %>%
+#         dplyr::select(place_id, place_rank) %>%
+#         tidyr::spread(place_rank, place_id)
+#     
+#     top_probs <- hprob %>% 
+#         mutate( prob_rank = paste0("P", rank)) %>%
+#         dplyr::select(P, prob_rank) %>%
+#         tidyr::spread(prob_rank, P)
+#     
+#     data.frame( h_x = hi, h_y = hj, P3cum = sum(hprob$P), n_places = n_places) %>%
+#         bind_cols( top_places, top_probs) 
+# }
